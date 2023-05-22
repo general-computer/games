d42582a15b257f563db97d64bf7619f9117aad07
Peter Myers <peter@pcmyers.com>
2023-05-21 22:25:39 -0400

    Add card and deck setup, and shuffling and dealing of cards

---
+++ poker/piston/v4/script.js
@@ -0,0 +1,48 @@
+<script>
+class Card {
+    constructor(suit, value) {
+        this.suit = suit;
+        this.value = value;
+    }
+}
+
+class Deck {
+    constructor() {
+        this.deck = [];
+        this.reset();
+        this.shuffle();
+    }
+    
+    reset() {
+        this.deck = [];
+        
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        
+        for (let suit in suits) {
+            for (let value in values) {
+                this.deck.push(new Card(suits[suit], values[value]));
+            }
+        }
+    }
+
+    shuffle() {
+        const { deck } = this;
+        let m = deck.length, i;
+
+        while (m) {
+            i = Math.floor(Math.random() * m--);
+            [deck[m], deck[i]] = [deck[i], deck[m]];
+        }
+
+        return this;
+    }
+
+    deal() {
+        return this.deck.pop();
+    }
+}
+
+let deck = new Deck();
+</script>
+
